55297fe106697ea3fb58e7b372a56a602e857191
Anchal <anchal15091995@gmail.com>
2020-02-09 17:34:55 +0530

    Added Welcome Annotations

---
+++ src/app/Shared/users.ts
@@ -0,0 +1,12 @@
+import { User } from "./user";
+
+export const USER: User[] = [
+    {
+        name: 'Anchal Gupta',
+        role: 'Administrator'
+    },
+    {
+        name: 'Sachin Dev Tomar',
+        role: 'Sub-Administrator'
+    }
+];
