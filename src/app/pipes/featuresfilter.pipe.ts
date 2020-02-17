17141d6af01c256618b4c94e0bfd7f6b70f43548
Sachin Dev Tomar <tomarsachindev@gmail.com>
2020-02-18 02:11:12 +0530

    Added filter on the features list using pipes

--- src/app/pipes/featuresfilter.pipe.ts
+++ src/app/pipes/featuresfilter.pipe.ts
@@ -5,6 +5,6 @@ export class FeaturesFilterPipe implements PipeTransform {
     transform(value: any[], args: string): any[] {
         let filterText: string = args ? args.toLocaleLowerCase() : null;
         return filterText ? value.filter((issue) =>
-            issue.name.toLocaleLowerCase().contains(filterText) != false) : value;
+            issue.Name.toLocaleLowerCase().includes(filterText) != false) : value;
     }
 }
