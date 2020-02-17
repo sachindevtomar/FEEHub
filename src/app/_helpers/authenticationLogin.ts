00f6a4570d3e960dcf6af3318b3fa6acd9b8de74
Sachin Dev Tomar <tomarsachindev@gmail.com>
2020-02-18 00:03:00 +0530

    Moved the interfaces into models folder and added table for features

--- src/app/_helpers/authenticationLogin.ts
+++ src/app/_helpers/authenticationLogin.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpRequest, HttpResponse, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable, of, throwError } from 'rxjs';
 import { delay, mergeMap, materialize, dematerialize } from 'rxjs/operators';
-import { User } from "../Shared/user";
+import { User } from "../models/user";
 import { USER } from "../Shared/users";
 
 let users = USER;
