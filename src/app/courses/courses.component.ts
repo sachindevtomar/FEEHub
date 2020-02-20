07989939f93d4244de1ecb071a93e72aace458ca
Anchal <anchal15091995@gmail.com>
2020-02-20 20:08:08 +0530

    Updated Course pagination to show 5 courses/page

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -9,17 +9,14 @@ import { COURSES } from "../Shared/courses";
 })
 export class CoursesComponent implements OnInit {
   
-  title = 'custom-pagination-angular';  
-  data = [];  
+  title = 'custom-pagination-angular'; 
   pagedItems: Array<any>;  
   courses = [];
-  //courses: Course[] =COURSES;
 
   constructor() { }
 
   ngOnInit() {
     this.courses = COURSES;
-    this.data = Array(1000).fill(0).map((x, i) => ({ id: (i + 1), name: `Item Paged ${i + 1}`, product: `Product ${i + 1}` }));  
   }
 
   beginPagination(pagedItems: Array<any>) {  
