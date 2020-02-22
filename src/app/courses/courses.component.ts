2ca950814887fa557a3d3b49ba51b73e4f6d0a51
Anchal <anchal15091995@gmail.com>
2020-02-22 19:03:27 +0530

    Added technologies by filtering

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -13,15 +13,29 @@ export class CoursesComponent implements OnInit {
   pagedItems: Array<any>;  
   courses = [];
   public courseSearchText: string = "";
+  public technologyTags : string[] = [];
 
   constructor() { }
 
   ngOnInit() {
     this.courses = COURSES;
+    this.collectAllTags();
   }
 
   beginPagination(pagedItems: Array<any>) {  
     this.pagedItems = pagedItems;  
   } 
 
+  collectAllTags(){
+    for(var i=0, j=0; i<COURSES.length; i++){
+      for(var k=0; k<COURSES[i].tags.length; k++)
+        this.technologyTags[j++] = COURSES[i].tags[k];
+    }
+    this.technologyTags = this.technologyTags.filter(function(item, pos, self) {
+      return self.indexOf(item) == pos;
+  });
+    this.technologyTags = this.technologyTags.sort();
+    console.log(this.technologyTags);
+    console.log(this.technologyTags.length);
+  }
 }
