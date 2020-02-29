c03ac038b3a6719547cdc34cdd4d8fe98b894dbb
Sachin Dev Tomar <tomarsachindev@gmail.com>
2020-02-29 17:04:12 +0530

    Added features

--- src/app/Shared/features.ts
+++ src/app/Shared/features.ts
@@ -1,37 +1,37 @@
-import {Feature} from '../models/feature';
+import { Feature } from '../models/feature';
 
 export const FEATURES: Feature[] = [
-    {
-        Name: 'Feature 1',
-    Description: 'Feature Description 1',
+  {
+    Name: 'Show Home Page',
+    Description: 'When it is enabled user can see Home page',
     CreatedDate: new Date("2019-01-16"),
     UpdatedDate: new Date("2019-01-18"),
     State: true
-    },
+  },
   {
-    Name: 'Feature 2',
-    Description: 'Feature Description 2',
+    Name: 'Show View Details button for courses',
+    Description: 'When it is enabled user can see View Details button for each course in Home page',
     CreatedDate: new Date("2019-05-17"),
     UpdatedDate: new Date("2019-06-18"),
     State: true
   },
   {
-    Name: 'Feature 3',
-    Description: 'Feature Description 3',
+    Name: 'Show courses page',
+    Description: 'When it is enabled user can see Courses page',
     CreatedDate: new Date("2019-09-19"),
     UpdatedDate: new Date("2019-10-20"),
     State: false
   },
   {
-    Name: 'Feature 4',
-    Description: 'Feature Description 4',
+    Name: 'Show courses filter',
+    Description: 'When it is enabled user can see Courses page filter',
     CreatedDate: new Date("2019-03-31"),
     UpdatedDate: new Date("2019-06-16"),
     State: false
   },
   {
-    Name: 'Feature 5',
-    Description: 'Feature Description 5',
+    Name: 'Show edit profile',
+    Description: 'When it is enabled user can see Edit profile page',
     CreatedDate: new Date("2020-01-14"),
     UpdatedDate: new Date("2020-02-09"),
     State: true
