ee851085f59b132222d026c08edf060dd567b327
Sachin Dev Tomar <tomarsachindev@gmail.com>
2020-03-01 14:43:35 +0530

    added claims in the user Object and claim protected home page

---
+++ src/app/_helpers/helper.methods.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {User} from "../models/user";
+
+@Injectable()
+export class HelperMethods{
+    public currentUser : User = null; 
+
+    constructor(){
+    }
+
+    public userHasClaim(claim: string): boolean{
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        console.log(this.currentUser);
+        if(this.currentUser.claims.includes(claim))
+            return true;
+        return false;
+    }
+}
