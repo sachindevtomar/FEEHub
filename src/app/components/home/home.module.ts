ddb17a7a95204a510fa7a8d965659624ba2fbe5b
Anchal <anchal15091995@gmail.com>
2020-03-09 12:03:17 +0530

    removed reference

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -5,7 +5,6 @@ import { HomeComponent } from './home.component';
 import { RouterModule } from '@angular/router';
 import { HomeRoutes } from './home.routes';
 import { HelperMethods } from "../../_helpers/helper.methods";
-//import { CoursedetailsComponent } from "../coursedetails/coursedetails.component";
 
 @NgModule({
     declarations: [HomeComponent],
