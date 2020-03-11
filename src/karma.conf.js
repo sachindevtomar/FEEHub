a5d3a2f02bca5b8b5f8aa721d1ec7955accce3fb
Sachin Dev Tomar <tomarsachindev@gmail.com>
2020-03-11 15:12:21 +0530

    updated karma settings to run the test cases on git build

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -25,7 +25,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    //later:
+    browsers: ['HeadlessChrome'],
+    customLaunchers:{
+      HeadlessChrome:{
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
+    singleRun: true
   });
 };
