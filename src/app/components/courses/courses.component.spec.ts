75b882649a487122123d21695c4a752c918f9d21
Anchal <anchal15091995@gmail.com>
2020-03-15 15:44:08 +0530

    Added more test cases for Course component

--- src/app/components/courses/courses.component.spec.ts
+++ src/app/components/courses/courses.component.spec.ts
@@ -51,26 +51,99 @@ describe('CoursesComponent', () => {
     });
   });
 
-  describe('showMoreTechnologies', () => {
+  describe('showMoreTechnologies method', () => {
     let showMoreDiv: HTMLDivElement;
     beforeEach(() => {
-       showMoreDiv = document.createElement('div');
-       showMoreDiv.id = "courses-filter-technology";
-       showMoreDiv.style.maxHeight = "300px";
-       component.courseFilterShowMore = false;
-       document.getElementById = jasmine.createSpy('HTML Element').and.returnValue(showMoreDiv);
+      showMoreDiv = document.createElement('div');
+      showMoreDiv.id = "courses-filter-technology";
+      showMoreDiv.style.maxHeight = "300px";
+      component.courseFilterShowMore = false;
+      document.getElementById = jasmine.createSpy('HTML Element').and.returnValue(showMoreDiv);
     });
 
-    it('should have maxHeight as 400px', () => {
+    it('should have maxHeight as 400px and overFlowY as scroll', () => {
       component.showMoreTechnologies();
       expect(showMoreDiv.style.maxHeight).toBe("400px");
       expect(showMoreDiv.style.overflowY).toBe("scroll");
     });
-    
+
     it('should have true courseFilterShowMore flag', () => {
       component.showMoreTechnologies();
       expect(component.courseFilterShowMore).toBeTruthy();
     });
   });
 
+  describe('showLessTechnologies method', () => {
+    let showLessDiv: HTMLDivElement;
+    beforeEach(() => {
+      showLessDiv = document.createElement('div');
+      showLessDiv.id = "courses-filter-technology";
+      showLessDiv.style.maxHeight = "300px";
+      component.courseFilterShowMore = false;
+      document.getElementById = jasmine.createSpy('HTML Element').and.returnValue(showLessDiv);
+    });
+
+    it('should have maxHeight as 200px and overFlowY as hidden', () => {
+      component.showLessTechnologies();
+      expect(showLessDiv.style.maxHeight).toBe("200px");
+      expect(showLessDiv.style.overflowY).toBe("hidden");
+    });
+
+    it('should have true courseFilterShowMore flag', () => {
+      component.showLessTechnologies();
+      expect(component.courseFilterShowMore).toBeTruthy();
+    });
+  });
+
+  describe('showMoreLessbutton method', () => {
+    beforeEach(() => {
+      component.courseShowMoreLessButton = false;
+    });
+
+    it('should have true courseShowMoreLessButton flag', () => {
+      component.showMoreLessbutton();
+      expect(component.courseShowMoreLessButton).toBeTruthy();
+    });
+  });
+
+  describe('collectAllTags method', () => {
+    it('should return 19 different technologies based on available tags in COURSES', () => {
+      component.collectAllTags();
+      expect(component.technologyTags.length).toBe(19);
+    });
+  });
+
+  describe('filterCoursesUsingTags method', () => {
+    beforeEach(() => {
+      component.selectedTags = ['A', 'B', 'C'];
+    });
+
+    it('should return length of selectedTags as 2, as A is already present in selectedTags array', () => {
+      component.filterCoursesUsingTags('A');
+      expect(component.selectedTags.length).toBe(2);
+    });
+
+    it('should return length of selectedTags as 4, as D is not present in selectedTags array', () => {
+      component.filterCoursesUsingTags('D');
+      expect(component.selectedTags.length).toBe(4);
+    });
+
+    it('should return length of filteredCoursesByTags as 0 if coursesAll doesn\'t has any value', () => {
+      component.selectedTags = [];
+      component.filterCoursesUsingTags('D');
+      expect(component.filteredCoursesByTags.length).toBe(0);
+    });
+
+    it('getCommonCoursesAfterFilter should have been called', () => {
+      component.filterCoursesUsingTags('A');
+      expect(component.getCommonCoursesAfterFilter).toHaveBeenCalled;
+    });
+  });
+
+  describe('filterCoursesUsingDuration method', () => {
+    it('getCommonCoursesAfterFilter should have been called', () => {
+      component.filterCoursesUsingDuration();
+      expect(component.getCommonCoursesAfterFilter).toHaveBeenCalled;
+    });
+  });
 });
